86ec2f2743b58f2ab4c15d62e8756754b532f0f2
AliBinary <AliGhanbariCs@gmail.com>
2024-12-16 23:28:30 +0330

    updated by ali

---
+++ Competitive Programming templates/Arshia Dadras.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void read_input()
+{
+}
+
+void solve()
+{
+}
+
+void write_output()
+{
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    read_input(), solve(), write_output();
+
+    return 0;
+}
